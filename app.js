433d40899372e090600eea09e3ffb088779a7085
amit15000 <135760038+amit15000@users.noreply.github.com>
2023-06-18 01:27:17 +0530

    Custom list --- error

--- app.js
+++ app.js
@@ -1,36 +1,24 @@
 const express = require("express");
-
 const app = express();
-// var ejs = require("ejs");
-
-// const date = require(__dirname+"/date.js");
-// console.log(date());
-
-app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
-const https = require('https')
-const _ =require("lodash")
-app.use(express.static("public"));
-
-
-//adding db
-
+const https = require('https');
+const _ = require("lodash");
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://Amit_15000:R7IxmCC3jZXqlQni@cluster0.zp9eqi6.mongodb.net/todolistDB");
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-//new item schema
+mongoose.connect("mongodb+srv://Amit_15000:R7IxmCC3jZXqlQni@cluster0.zp9eqi6.mongodb.net/todolistDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
 const itemsSchema = {
   name: String,
 };
 
-// const itemSchema = new mongoose.Schema({
-//     name: String
-//   });
-
-const Item = mongoose.model("Item", itemsSchema); //--using it bcz we are using database
-
+const Item = mongoose.model("Item", itemsSchema);
 const item1 = new Item({
   name: "Welcome to your todoList",
 });
@@ -49,27 +37,13 @@ const listSchema = {
 };
 const List = mongoose.model("List", listSchema);
 
-// const newUser = new User({ name: 'John Doe', age: 25, email: 'johndoe@example.com' });
-// newUser.save()
-//   .then((result) => {
-//     console.log('User created:', result);
-//   })
-//   .catch((error) => {
-//     console.error('Error creating user:', error);
-//   });
-
-// // let queries="";    esse bas last mein change aata rahega --aage nahi barega esliye query ke place pe ab items us kar raha
-// const items=["Buy Food","Cook Food","Eat Food"];
-// //const array mein push ho sakta hai javascript special
-// var workItems=[];
-
 app.get("/", async function (req, res) {
   try {
     const foundItems = await Item.find({});
     if (foundItems.length === 0) {
       Item.insertMany(defaultItems)
         .then((result) => {
-          console.log("Default Items inserted :", result);
+          console.log("Default Items inserted:", result);
         })
         .catch((error) => {
           console.error("Error inserting items:", error);
@@ -83,13 +57,8 @@ app.get("/", async function (req, res) {
   }
 });
 
-
-//custom route
-
 app.get("/:customListName", (req, res) => {
-  const customListName = _.capitalize(req.params.customListName); // Access the value of the 'customListName' parameter
-  // console.log("Dynamic route:", customListName); // Output the dynamic route to the console
-  // res.send(`Dynamic route: ${customListName}`);
+  const customListName = _.capitalize(req.params.customListName);
 
   List.findOne({ name: customListName })
     .then((foundList) => {
@@ -100,78 +69,61 @@ app.get("/:customListName", (req, res) => {
           name: customListName,
           items: defaultItems,
         });
-        newList.save()
-          res.redirect( '/'+customListName);
+        newList.save();
+        res.redirect('/' + customListName);
       }
     })
-    .catch((error)  => {
+    .catch((error) => {
       console.error("Error finding dynamic route:", error);
     });
 });
-    
-
-
 
 app.post("/", function (req, res) {
   let itemName = req.body.newItem;
   const listName = req.body.list;
 
-
   const item = new Item({
     name: itemName,
   });
 
-
-  if(listName === "Today"){
+  if (listName === "Today") {
     item.save();
-  res.redirect("/");
-}
-else{
-
-  // findOne function ka return then function ke variable ( here -- foundlist) mein aata hai ---- foundOne returns Null or found list document.
-  List.findOne({ name: listName })
-    .then((foundList) => {
-      if (foundList) {
-       foundList.items.push(item)      //ex Home route ke andar schema mein items array mein add karna hai
-        foundList.save();
-        res.redirect('/'+ listName)
-      } 
-    })
-    .catch((error) => {
-      console.error("Error finding dynamic route:", error);
-      res.send("An error occurred while finding the dynamic route."); // Send an error response if there's an issue with finding the dynamic route
-    });
-}
-
-   
-
- 
+    res.redirect("/");
+  } else {
+    List.findOne({ name: listName })
+      .then((foundList) => {
+        if (foundList) {
+          foundList.items.push(item);
+          foundList.save();
+          res.redirect('/' + listName);
+        }
+      })
+      .catch((error) => {
+        console.error("Error finding dynamic route:", error);
+        res.send("An error occurred while finding the dynamic route.");
+      });
+  }
 });
 
 app.post("/delete", function (req, res) {
   const itemId = req.body.checkbox;
   const listName = req.body.listName;
 
-
-  if(listName === "Today"){
+  if (listName === "Today") {
     Item.findByIdAndRemove(itemId)
-    .then((deletedItem) => {
-      if (deletedItem) {
-        console.log("Item deleted successfully:", deletedItem);
-      } else {
-        console.log("Item not found.");
-      }
-      res.redirect('/');
-    })
-  }
-  else{
+      .then((deletedItem) => {
+        if (deletedItem) {
+          console.log("Item deleted successfully:", deletedItem);
+        } else {
+          console.log("Item not found.");
+        }
+        res.redirect('/');
+      });
+  } else {
     List.findOneAndUpdate({ name: listName }, { $pull: { items: { _id: itemId } } })
       .then(() => {
         res.redirect('/' + listName);
-      })
-
-
-
+      });
   }
 });
 
@@ -179,4 +131,4 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(process.env.PORT || 3000, () => console.log(`TodoList Server Running Sucessfully`));
+app.listen(process.env.PORT || 3000, () => console.log("TodoList Server Running Successfully"));
